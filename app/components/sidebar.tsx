7c559f11a5d44fd982572137032d6bbf61d79711
上善若水 <119553246+ssrsyds@users.noreply.github.com>
2023-06-22 10:31:40 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Plus</div>
+        <div className={styles["sidebar-title"]}>ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
           Welcome,Your Exclusive Ai Assistant.
         </div>
